2ef8b303582d903fb2990963783f3653e8a70550
FricoSimon <simonfriko@gmail.com>
2023-04-27 11:04:58 +0700

    add gitignore and update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.63" //specify the version
+    }
+  }
+}
+provider "google" {
+  project = "latihanfrikogcp" //your project ID
+  region  = "asia-southeast2"
+  zone    = "asia-southeast2-a"
+}
 resource "google_compute_instance" "VM" {
-  name = "Machine-1" //Name of the VM
+  name = "machine1" //Name of the VM
   machine_type = "e2-micro" 
   zone = "asia-southeast2-a" //Jakarta
+  tags = [ "http-server" ]
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts" //Ubuntu 18.04
